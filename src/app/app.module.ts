ad1d6ff7711350ccb44507d65759f66f0ed165ca
Hariettemaina <hariette.maina@student.moringaschool.com>
2022-04-09 23:23:31 +0300

    added highligt directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormComponent } from './form/form.component';
 import { QuoteComponent } from './quote/quote.component';
+import { HighlightQuoteDirective } from './highlight-quote.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     FormComponent,
-    QuoteComponent
+    QuoteComponent,
+    HighlightQuoteDirective
   ],
   imports: [
     BrowserModule,
