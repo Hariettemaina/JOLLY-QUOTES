27708c1ef783c4cf8ac4b92b5754105c9f12564a
Hariettemaina <hariette.maina@student.moringaschool.com>
2022-04-09 23:26:09 +0300

    added component details

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { FormComponent } from './form/form.component';
 import { QuoteComponent } from './quote/quote.component';
 import { HighlightQuoteDirective } from './highlight-quote.directive';
+import { QuoteDetailsComponent } from './quote-details/quote-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FormComponent,
     QuoteComponent,
-    HighlightQuoteDirective
+    HighlightQuoteDirective,
+    QuoteDetailsComponent
   ],
   imports: [
     BrowserModule,
